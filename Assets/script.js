c06d64c6adf84d0417258d67d37d1eb9bd5c6d48
avaarm <armenuhiavanesyan@gmail.com>
2020-03-30 21:16:07 -0700

    fixing event listener

---
+++ Assets/script.js
@@ -0,0 +1,182 @@
+// Variable set for timer, event listener, quiz Q and A
+var remainingSeconds = 60;
+var timerEl = document.querySelector(".timer");
+var timerStart;
+var start = document.querySelector(".startQuiz");
+var buttons = document.querySelector(".buttons");
+var container = document.querySelector(".container");
+var submit = document.querySelector(".submit");
+var scores = [];
+var save = [];
+var highScoresEl = document.querySelector(".highScoresLink");
+var clear = document.querySelector(".clear");
+var form = document.querySelector(".submitForm");
+var index = 0;
+var score = 0;
+
+
+// Using addEventListener on "Start Quiz" button to start the timer:
+function startQuiz() {
+    start.addEventListener("click", function(event) {
+      if (remainingSeconds < 60) {
+        event.preventDefault();
+      } else {
+        timerEl.textContent = "Time: " + remainingSeconds; 
+        timer();
+        insertQuestion();
+      }
+    });
+  }
+
+
+// Use function timer() to countdown from remainingSeconds:
+function timer() {
+    var timerInterval = setInterval(function() {
+      remainingSeconds--;
+      timerEl.textContent = "Time: " + remainingSeconds;
+      if (remainingSeconds <= 0) {
+        clearInterval(timerInterval);
+        timerEl.textContent = "Time's up!";
+      }
+    }, 1000);
+  }
+
+// Set an array as a variable that includes questions, answers, and correct answers:
+var arrayofQuestions = [
+  {
+    question: "Question 1: Commonly used data types DO NOT include:",
+    answers: [
+      "a. strings",
+      "b. booleans",
+      "c. alerts", 
+      "d. numbers"
+    ],
+    rightAnswer: "c. alerts"
+  },
+  {
+    question:
+      "Question 2: The condition of an if / else statement is enclosed within ________",
+    answers: [
+      "a. quotes",
+      "b. curly brackets",
+      "c. parentheses", 
+      "d. square brackets"
+    ],
+    rightAnswer: "c. parentheses"
+  },
+  {
+    question: "Question 3: Arrays in JavaScript can be used to store ________",
+    answers: [
+      "a. numbers and strings",
+      "b. other arrays",
+      "c. booleans",
+      "d. all of the above"
+    ], 
+    rightAnswer: "d. all of the above"
+  },
+  {
+    question:
+      "Question 4: String values must be enclosed within ________ when being assigned to variables.",
+    answers: ["a. commas", "b. curly brackets", "c. quotes", "d. parentheses"], //correct
+    rightAnswer: "d. parentheses"
+  },
+  {
+    question:
+      "Question 5: A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      "a. JavaScript",
+      "b. terminal/bash",
+      "c. for loops",
+      "d. console.log"
+    ],
+    rightAnswer: "c. for loops"
+  }
+];
+
+// In order for the user to be prompted with a questions and for the questions to be in a "new screen"
+function insertQuestion() {
+  container.innerHTML = "<h2>" + arrayofQuestions[index].question + "</h2>";
+  container.style.textAlign = "left";
+  for (var i = 0; i < arrayofQuestions[index].answers.length; i++) {
+    document.querySelector(".answer" + i).innerHTML =
+      "<button type='button' class='btn btn-primary'>" +
+      arrayofQuestions[index].answers[i] +
+      "</button>";
+  }
+}
+// Adding an event listener for the right answer choice/ button 
+buttons.addEventListener("click", function(event) {
+  event.preventDefault();
+ 
+
+  if (event.target.textContent === arrayofQuestions[index].rightAnswer) {
+    score = score + 10;
+  } else {
+    remainingSeconds = remainingSeconds - 10;
+  }
+  if (index < arrayofQuestions.length - 1) {
+    index++;
+    insertQuestion();
+  } else {
+    remainingSeconds = 0;
+    enterScore();
+  }
+});
+
+function correctAnswer() {
+  return;
+}
+
+function enterScore() {
+  container.innerHTML = "<h2>All Done!</h2><br>";
+  buttons.innerHTML = "Your final score is " + score + "." + "<br><br>";
+    var form = document.createElement("form");
+    submit.appendChild(form);
+    innerHTML = enterInitials;
+    var initialsButton = document.querySelector(".initialButton");
+  initialsButton.addEventListener("click", function(event) {
+
+    event.preventDefault(); 
+    if (localStorage.getItem("user,score") !== null) {
+      scores = JSON.parse(localStorage.getItem("user,score"));
+    }
+
+    if (scores.length > 0) {
+      for (var b = 0; b < scores.length; b++) {
+        save.push(scores[b]);
+      }
+    }
+
+
+    var userInitials = document.querySelector(".initInput").value;
+    var userScore = { userInitials, score };
+
+    save.push(userScore);
+
+    localStorage.setItem("user,score", JSON.stringify(save));
+
+    viewScore();
+  });
+}
+
+function viewScore() {
+  container.innerHTML = "<h2>High Scores!</h2><br>";
+  for (var t = 0; t < save.length; t++) {
+    var scoreList = document.createElement("div");
+    scoreList.textContent =
+      "Usser: " +
+      save[t].userInitials +
+      "-----------------" +
+      "   Score: " +
+      save[t].score;
+    container.appendChild(scoreList);
+  }
+  buttons.innerHTML = "<button type='button' class='btn btn-primary'>" + "Clear Scores" +
+  "</button>";
+  form.innerHTML = "<div></div>";
+}
+
+
+
+startQuiz();
+
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # 04-Code-Quiz
 A javascript timed code quiz
+
+
+
+As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+## User Story
+
+```
+AS A coding bootcamp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+Notes from Chris
+an array of of objects 
+    - (need to make buttons)
+    - with questions, keep repeating the process until we are out of questions
+
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+```
+
+**NOTE: Do not use jQuery! Please use plain, vanilla Javascript for this**
+
+The following animation demonstrates the application functionality:
+
+![code quiz](./Assets/04-web-apis-homework-demo.gif)
+
+### Review
+
+You are required to submit the following for review:
+
+- The URL of the functional, deployed application.
+
+- The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+### Hints
+
+- You will need to use `setInterval` for the timer.
+- You will need to use `localStorage`. What is the part of app that needs to persist?
+- You will need use event delegation.
+- The Week 4 ToDo's application (`28-Stu_Local-Storage-Todos`) has examples of `localStorage` and event delegation.
+
+---
+
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
